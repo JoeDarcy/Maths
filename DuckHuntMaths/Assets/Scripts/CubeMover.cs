3c8f3d1833dc623b8c928aa0528690ec064b183f
JoeDarcy <59360012+JoeDarcy@users.noreply.github.com>
2020-11-19 04:05:32 +0000

    All scripts for vectors and vvector based movement add
    All custom vector scripts added and movement script defined. Cube now moves according to cube mover script variables, (Vec3).

---
+++ DuckHuntMaths/Assets/Scripts/CubeMover.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMover : MonoBehaviour
+{
+    //axis (basis) vectors for cube
+    private Vec3 xAxis = new Vec3( 1f, 0f, 0f);
+    private Vec3 yAxis = new Vec3( 0f, 1f, 0f);
+
+    private Vec3 position = new Vec3( 0f, 0f, 0f);
+
+    public Vec3 Velocity = new Vec3( 0f, 0f, 0f);
+
+    public Vec3 Acceleration = new Vec3(0f, 12f, 7f);
+
+    public float duration = 1.0f;
+    private bool renewDuration = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //draws basic lines along our defined axis
+    private void OnDrawGizmos() {
+        //Draw X Axis in Red
+       Gizmos.color = Color.red;
+       Gizmos.DrawLine(position.ToVector3(), (position + (xAxis * 3) ).ToVector3());
+
+       //Draw Y Axis in Green
+       Gizmos.color = Color.green;
+       Gizmos.DrawLine(position.ToVector3(), (position + (yAxis * 3) ).ToVector3());
+
+       //Produce Z Axis from CrossProduct of X Axis and Y Axis and Colour it Blue
+       Gizmos.color = Color.blue;
+       Vec3 zAxis = Vec3.CrossProduct(xAxis, yAxis); 
+
+       //axis produced should be perpendicular to plane created by vectors xAxis and yAxis 
+       Gizmos.DrawLine(position.ToVector3(), (position + (zAxis * 3) ).ToVector3());
+
+       //draw velocity vector as a line
+       Gizmos.color = Color.white;
+       Gizmos.DrawLine(position.ToVector3(), (position + Velocity).ToVector3());
+       
+    }
+
+    private Vec3 calculateDisplacement( Vec3 a_velocity, float a_deltaTime){
+        return a_velocity * a_deltaTime;
+    }
+
+    private Vec3 calculatePosition( Vec3 a_initPos, Vec3 a_velocity, float a_deltaTime){
+        return a_initPos + calculateDisplacement(a_velocity, a_deltaTime);
+    }
+
+    private Vec3 calculateAverageVelocity( Vec3 a_initPos, Vec3 a_finalPos, float a_deltaTime){
+        if( a_deltaTime > 0f){
+            return (a_finalPos - a_initPos) / a_deltaTime; 
+        }
+        return new Vec3(0f, 0f, 0f);
+    }
+
+    private Vec3 calulateAcceleration( Vec3 a_initVel, Vec3 a_finalVel, float a_deltaTime){
+        if( a_deltaTime > 0f){
+            return (a_finalVel - a_initVel) / a_deltaTime;
+        }
+        return new Vec3( 0f, 0f, 0f);
+    }
+
+    private Vec3 calculateVelocity( Vec3 a_acceleration, float a_deltaTime){
+        return a_acceleration * a_deltaTime;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if( position.y < 0f)
+        {
+            Velocity = new Vec3(0f, 0f, 0f);
+            Acceleration = new Vec3(0f, 0f, 0f);
+        }
+        //calculate velocity based off accelleration
+        Velocity += calculateVelocity( Acceleration, Time.deltaTime);
+        //change in displacement is velocity multiplied by time
+        //position + change in displacement is the new position
+        Vec3 displacement = calculateDisplacement(Velocity , Time.deltaTime);
+        Debug.Log(displacement.ToString());
+        position += displacement;
+        //set positon variable back into transform.position
+        transform.position = position.ToVector3();
+    
+        if( duration < 0f && renewDuration ){
+            Acceleration = new Vec3(0f, -9.8f, 0f);
+            renewDuration = false;
+        }
+        duration -= Time.deltaTime;
+        
+    }
+}
---
+++ DuckHuntMaths/Assets/Scripts/Vec3.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vec3
+{
+    public float x = 0f;
+    public float y = 0f;
+    public float z = 0f;
+
+    public Vec3(){
+        x = 0f; y = 0f; z = 0f;
+    }
+
+    public Vec3( float a_x, float a_y, float a_z){
+        x = a_x; y = a_y; z = a_z;
+    }
+
+    //convert from our Vec2 custom data structure to Unity's Vector3 data structure
+    public Vector3 ToVector3(){
+        return new Vector3(x, y, z);
+    }
+
+    public override string ToString(){
+        return "Vec3: " + x.ToString() + "X " + y.ToString() + "Y " + z.ToString() + "Z";
+    }
+    
+    //overload the add (+) operator
+    public static Vec3 operator +( Vec3 a, Vec3 b){
+        return new Vec3( a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static Vec3 operator -( Vec3 a){
+        return new Vec3( -a.x, -a.y, -a.z);
+    }
+
+    public static Vec3 operator -( Vec3 a, Vec3 b){
+        return new Vec3( a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static Vec3 operator *( Vec3 a, Vec3 b)
+    {
+        return new Vec3( a.x * b.x, a.y * b.y, a.z * b.z);
+    }
+
+    public static Vec3 operator *( Vec3 a, float b)
+    {
+        return new Vec3( a.x * b, a.y * b, a.z * b);
+    }
+
+    public static Vec3 operator /( Vec3 a, float b)
+    {
+        return new Vec3( a.x / b, a.y / b, a.z / b);
+    }
+
+    public float Magnitude(){
+        return Mathf.Sqrt(x * x + y * y + z * z);
+    }
+
+    public float MagnitudeSquared(){
+        return x * x + y * y + z * z;
+    }
+
+    public float DotProduct( Vec3 a_b )
+    {
+        return x * a_b.x + y * a_b.y + z * a_b.z;
+    }
+
+    public static float DotProduct( Vec3 a, Vec3 b )
+    {
+        return a.x * b.x + a.y * b.y + a.z * b.z;
+    }
+
+    /// Normalize 
+    ///     This function transforms the vector into a unit length vector
+    ///     A Unit vector has a length of 1.0
+    ///     It is possible to use scientific notation within C#
+    ///     Return the original length of the vector before normalisation
+    public float Normalize()
+    {
+        float fMag = Magnitude();
+        float fInvMag = (fMag != 0f) ? 1f/fMag : 1.00e+12f;
+        x *= fInvMag;
+        y *= fInvMag;
+        z *= fInvMag;
+        return fMag;
+
+    }
+
+    public static Vec3 CrossProduct( Vec3 a, Vec3 b)
+    {
+        return new Vec3( a.y*b.z - a.z*b.y,
+                         a.z*b.x - a.x*b.z,
+                         a.x*b.y - a.y*b.x);
+    }
+
+    public void RotateX( float angle )
+    {
+        float fY = y;
+        y = fY * Mathf.Cos(angle) - z * Mathf.Sin(angle);
+        z = fY * Mathf.Sin(angle) + z * Mathf.Cos(angle);
+    }
+
+    public void RotateY( float angle )
+    {
+        float fX = x;
+        x = fX * Mathf.Cos(angle) - z * Mathf.Sin(angle);
+        z = fX * Mathf.Sin(angle) + z * Mathf.Cos(angle);
+    }
+
+    public void RotateZ( float angle )
+    {
+        float fX = x;
+        x = fX * Mathf.Cos(angle) - y * Mathf.Sin(angle);
+        y = fX * Mathf.Sin(angle) + y * Mathf.Cos(angle);
+    }
+
+}
